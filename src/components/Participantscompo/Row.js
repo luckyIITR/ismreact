c7a2ffa6d5dcea77ee43faee6fca17dc8100c4da
luckyIITR <luckykushwaha56@gmail.com>
2021-09-07 10:16:50 +0530

    modified something

--- src/components/Participantscompo/Row.js
+++ src/components/Participantscompo/Row.js
@@ -3,19 +3,20 @@ import React from 'react';
 function Row(props) {
     return (
         <div className="row-table">
-            <div className="cell" data-title="Full Name">
+            <div className="cell" data-title="S.No.">
                 {props.props.index + 1}
             </div>
-            <div className="cell" data-title="Age">
+            <div className="cell" data-title="Name">
                 {props.props.row.name}
             </div>
-            <div className="cell" data-title="Job Title">
+            <div className="cell" data-title="Student/Faculty">
                 {props.props.row.post}
             </div>
-            <div className="cell" data-title="Location">
+            <div className="cell" data-title="Instiution">
                 {props.props.row.institute}
             </div>
         </div>
     )
 }
+
 export default Row;
