d2fbe6c16f299e5571c51837a8be88b49b861dab
Lucky Kushwaha <Luckykushwaha56@gmail.com>
2021-10-18 11:45:26 +0530

    speaker updated

--- src/components/Schedulecompo/Speaker.js
+++ src/components/Schedulecompo/Speaker.js
@@ -19,37 +19,44 @@ function Speaker(props) {
 
                             </p>
                             <p>{props.props.institute}</p>
-                            {(props.props.file || props.props.youtube) &&
+
                             <div style={{display: "flex", flexWrap: "wrap"}}>
-                                <div className="lect">
-                                    <a className="ui button outline lifeAtIITRButton" href={props.props.file} target={'_blank'}
-                                       rel="noopener noreferrer"><i
-                                        className="fas fa-file-pdf"
-                                        style={{color: "red"}}> </i>&nbsp;&nbsp; Lecture File</a>
-                                </div>
+
+                                {
+                                    props.props.youtube &&
                                 <div className="lect">
                                     <a className="ui button outline lifeAtIITRButton" href={props.props.youtube}
                                        target={'_blank'} rel="noopener noreferrer"><i
                                         className="fab fa-youtube"
                                         style={{color: "red"}}> </i>&nbsp;&nbsp; YouTube</a>
                                 </div>
+                                }
+                                {props.props.file &&
+
+                                <div className="lect">
+                                    <a className="ui button outline lifeAtIITRButton" href={props.props.file} target={'_blank'}
+                                       rel="noopener noreferrer"><i
+                                        className="fas fa-file-pdf"
+                                        style={{color: "red"}}> </i>&nbsp;&nbsp; Talk File</a>
+                                </div>
+                                }
                             </div>
-                            }
+
 
                         </div>
-                        {props.props.time &&
+                        {props.props.title &&
                         <div className="sch-col-2">
                             <div style={{display: 'flex', justifyContent:"flex-end"}}>
-                                <div className="time"><i className="fas fa-clock"> </i>&nbsp; {props.props.time}</div>
+                                {
+                                    props.props.time &&
+                                    <div className="time"><i className="fas fa-clock"> </i>&nbsp; {props.props.time}</div>
+                                }
+
                             </div>
                             <br/>
-
-                            {
-                                props.props.title &&
                                     <div style={{textAlign:"center",display: 'flex',justifyContent:"center", fontSize:"1.5rem", fontWeight:'bold'}}>
                                         {props.props.title}
                                     </div>
-                            }
                         </div>
                         }
                     </div>
