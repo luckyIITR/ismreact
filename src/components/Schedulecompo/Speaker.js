b1320372d069b6798de558146fa1c9f218d17007
lucky <luckykushwaha56@gmail.com>
2021-11-27 15:10:53 +0530

    final update

--- src/components/Schedulecompo/Speaker.js
+++ src/components/Schedulecompo/Speaker.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+const style_ele = {pointerEvents:"None"};
+
 function Speaker(props) {
     return (
         <div className="main-page-noticeboard">
@@ -20,43 +22,48 @@ function Speaker(props) {
                             </p>
                             <p>{props.props.institute}</p>
 
-                            <div style={{display: "flex", flexWrap: "wrap"}}>
-
-                                {
-                                    props.props.youtube &&
-                                <div className="lect">
-                                    <a className="ui button outline lifeAtIITRButton" href={props.props.youtube}
-                                       target={'_blank'} rel="noopener noreferrer"><i
-                                        className="fab fa-youtube"
-                                        style={{color: "red"}}> </i>&nbsp;&nbsp; YouTube</a>
-                                </div>
-                                }
-                                {props.props.file &&
-
-                                <div className="lect">
-                                    <a className="ui button outline lifeAtIITRButton" href={props.props.file} target={'_blank'}
-                                       rel="noopener noreferrer"><i
-                                        className="fas fa-file-pdf"
-                                        style={{color: "red"}}> </i>&nbsp;&nbsp; Talk File</a>
-                                </div>
-                                }
-                            </div>
-
-
-                        </div>
-                        {props.props.title &&
-                        <div className="sch-col-2">
-                            <div style={{display: 'flex', justifyContent:"flex-end"}}>
+                            <div style={{marginTop: "0.5em"}}>
                                 {
                                     props.props.time &&
                                     <div className="time"><i className="fas fa-clock"> </i>&nbsp; {props.props.time}</div>
                                 }
 
                             </div>
-                            <br/>
-                                    <div style={{textAlign:"center",display: 'flex',justifyContent:"center", fontSize:"1.5rem", fontWeight:'bold'}}>
+
+
+
+
+                        </div>
+                        {props.props.title &&
+                        <div className="sch-col-2">
+
+                                    <div style={{textAlign:"center",display: 'flex',justifyContent:"center", fontSize:"1.5rem", fontWeight:'bold', margin: "0.5em auto"}}>
                                         {props.props.title}
+
                                     </div>
+
+                            <div style={{display: "flex", flexWrap: "wrap", justifyContent : "center"}}>
+
+                                {
+                                    props.props.youtube &&
+                                    <div className="lect">
+                                        <a className="ui button outline lifeAtIITRButton" href={props.props.youtube}
+                                           target={'_blank'} rel="noopener noreferrer" style={props.props.youtube === "-"? style_ele : {}}><i
+                                            className="fab fa-youtube"
+                                            style={{color: "red"}}> </i>&nbsp;&nbsp; YouTube</a>
+                                    </div>
+                                }
+                                {props.props.file &&
+
+                                <div className="lect">
+                                    <a className="ui button outline lifeAtIITRButton" href={props.props.file} target={'_blank'}
+                                       rel="noopener noreferrer" style={props.props.file === "-"? style_ele : {}}><i
+                                        className="fas fa-file-pdf"
+                                        style={{color: "red"}}> </i>&nbsp;&nbsp; Talk File</a>
+                                </div>
+                                }
+                            </div>
+
                         </div>
                         }
                     </div>
--- src/components/Schedulecompo/Timetable.js
+++ src/components/Schedulecompo/Timetable.js
@@ -80,7 +80,7 @@ function Timetable() {
                                     <td > Sukrut Mondkar</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row" rowSpan="2">12:30-1:00pm</th>
+                                    <th scope="row" rowSpan="2">12:30-13:00</th>
                                     <td > Renjan R. John</td>
                                     <td > Ayan K. Patra</td>
                                     <td > Madhu Mishra</td>
@@ -97,12 +97,12 @@ function Timetable() {
                                 </tr>
 
                                 <tr style={{background: "rgba(229, 238, 244, 0.6)"}}>
-                                    <th scope="row">1:00-2:00pm</th>
+                                    <th scope="row">13:00-14:00</th>
                                     <td colSpan="6">Lunch Break</td>
 
                                 </tr>
                                 <tr>
-                                    <th scope="row">2:00-2:30pm</th>
+                                    <th scope="row">14:00-14:30</th>
                                     <td>Abhishek Mehta</td>
                                     <td rowSpan="3"> Laura Donnay</td>
                                     <td>Bindusar Sahoo</td>
@@ -111,7 +111,7 @@ function Timetable() {
                                     <td rowSpan="2"> Joao Penedones</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row" rowSpan="2">2:30-3:00pm</th>
+                                    <th scope="row" rowSpan="2">14:30-15:00</th>
                                     <td rowSpan="2">Arnab Kundu</td>
                                     <td rowSpan="2">Suresh Govindarajan</td>
                                     <td>Aritra Ghosh</td>
@@ -120,7 +120,7 @@ function Timetable() {
                                     <td>Debangshu Mukherjee</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row" rowSpan="2">3:00-3:30pm</th>
+                                    <th scope="row" rowSpan="2">15:00-15:30</th>
                                     <td rowSpan="2">Ayan Mukhopadhyay</td>
                                     <td rowSpan="2">Arindam Bhattacharjee</td>
                                     <td rowSpan="2">Chetan Gowdigere </td>
@@ -132,11 +132,11 @@ function Timetable() {
                                     <td>Himanshu Parihar</td>
                                 </tr>
                                 <tr style={{background: "rgba(229, 238, 244, 0.6)"}}>
-                                    <th scope="row">3:30-4:00</th>
+                                    <th scope="row">15:30-16:00</th>
                                     <td colSpan="6">Tea Break</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">4:00-4:30</th>
+                                    <th scope="row">16:00-16:30</th>
                                     <td>Tadashi Takayanagi</td>
                                     <td>Mangesh Mandlik</td>
                                     <td>R. Loganayagam</td>
@@ -145,7 +145,7 @@ function Timetable() {
                                     <td>Ronak Soni</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">4:30-5:00</th>
+                                    <th scope="row">16:30-17:00</th>
                                     <td>Krishnan Narayan</td>
                                     <td>Aritra Banerjee</td>
                                     <td>Debajyoti Sarkar</td>
@@ -154,7 +154,7 @@ function Timetable() {
                                     <td>Siddharth Prabhu</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">5:00-5:30</th>
+                                    <th scope="row">17:00-17:30</th>
                                     <td>Ahmadullah Zahed</td>
                                     <td>Kushal Chakraborty</td>
                                     <td>Karunava Sil</td>
@@ -162,7 +162,7 @@ function Timetable() {
                                     <td>Aalok Misra</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">5:30-6:00</th>
+                                    <th scope="row">17:30-18:00</th>
                                     <td rowSpan="2"> Discussion session</td>
                                     <td rowSpan="2"> Discussion session</td>
                                     <td >Zohar Komargodski</td>
@@ -171,22 +171,27 @@ function Timetable() {
                                     <td> Discussion session</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">6:00-6:30</th>
-                                    <td></td>
+                                    <th scope="row">18:00-18:30</th>
+                                    <td> Discussion session</td>
+                                    <td>Break</td>
                                     <td>Break</td>
                                 </tr>
                                 <tr>
-                                    <th scope="row">6:30-7:30</th>
+                                    <th scope="row">18:30-19:00</th>
                                     <td rowSpan="2"/>
                                     <td rowSpan="2"/>
-                                    <td rowSpan="2"/>
-                                    <td>Daniel Harlow</td>
+                                    <td rowSpan="2">Daniel Harlow</td>
+                                    <td>Ahmed Almheiri</td>
                                     <td rowSpan="2">Davide Gaiotto</td>
                                     <td rowSpan="2"/>
                                 </tr>
                                 <tr>
-                                    <th scope="row">7:30-8:00</th>
-                                    <td>Ahmed Almheiri</td>
+                                    <th scope="row">19:00-19:30</th>
+
+                                </tr>
+                                <tr>
+                                    <th scope="row">19:30-20:00</th>
+
                                 </tr>
                                 </tbody>
                             </table>
