75dcaacd8a3b106c91d42b40097e89d367210c87
lucky <luckykushwaha56@gmail.com>
2021-11-30 14:00:38 +0530

    Revert "timetable updated"
    This reverts commit ff0d746e0096e2280ffc78c166f31ccf37e00a81.

--- src/components/Schedulecompo/Timetable.js
+++ src/components/Schedulecompo/Timetable.js
@@ -3,7 +3,7 @@ import './table.scoped.css'
 function Timetable() {
     return (
         <section className="ftco-section">
-            <div className="container-timetable container">
+            <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-6 text-center mb-5">
                         <h2 className="heading-section">SCHEDULE</h2>
@@ -26,192 +26,153 @@ function Timetable() {
                                 </thead>
                                 <tbody>
                                 <tr>
-                                    <th scope="row" style={{fontWeight: "1000"}}>Session Chairs</th>
-                                    <td style={{fontWeight: "1000"}}> Spenta Wadia</td>
-                                    <td style={{fontWeight: "1000"}}>Ramadevi</td>
-                                    <td style={{fontWeight: "1000"}}>Anirban Basu</td>
-                                    <td/>
-                                    <td style={{fontWeight: "1000"}}>Nabamita Banerjee</td>
-                                    <td style={{fontWeight: "1000"}}>Harvendra Singh</td>
-                                </tr>
-                                <tr>
                                     <th scope="row">9:30-10:00</th>
-                                    <td rowSpan="2"> Rajesh Gopakumar (Dual of Free SYM)</td>
-                                    <td> Sergei Gukov (Higher-form symmetries in TQFTs)</td>
-                                    <td >Ashoke Sen (D-instanton corr. to string ampli.)</td>
+                                    <td rowSpan="2"> Rajesh Gopakumar</td>
+                                    <td> Sergei Gukov</td>
+                                    <td > Ashoke Sen</td>
                                     <td rowSpan="3"> Free Morning</td>
-                                    <td> Akshay Yelleshpur (Deformed W algebras)</td>
-                                    <td> Sandipan Kundu (Extremal Chaos)</td>
+                                    <td> Akshay Yelleshpur</td>
+                                    <td> Sandipan Kundu</td>
                                 </tr>
                                 <tr>
                                     <th scope="row">10:00-10:30</th>
-                                    <td>Sujay Ashok (AdS3*S1 Chiral Ring)</td>
-                                    <td>Shiraz Minwalla/his substitute (Hilbert space – Matter CS)</td>
+                                    <td>Sujay Ashok</td>
+                                    <td>Shiraz Minwalla/his substitute</td>
                                     <td>Koushik Ray</td>
-                                    <td>Pratik Rath (Tensor Networks and Holography)</td>
+                                    <td>Pratik Rath</td>
                                 </tr>
                                 <tr>
                                     <th scope="row">10:30-11:00</th>
                                     <td>Chetan Krishnan</td>
-                                    <td>Semanti Dutta (Bulk Gauge Fields and Holographic RG)</td>
-                                    <td>Onkar Parrikar (QEC in BH Interior)</td>
+                                    <td>Semanti Dutta</td>
+                                    <td>Onkar Parrikar</td>
                                     <td>Arnab Rudra</td>
-                                    <td>Joydeep Chakravarty (Monogamy, bags of gold paradoxes)</td>
+                                    <td>Joydeep Chakravarty</td>
                                 </tr>
                                 <tr style={{background: "rgba(229, 238, 244, 0.6)"}}>
                                     <th scope="row">11:00-11:30</th>
                                     <td colSpan="6">Tea Break</td>
-                                </tr>
-                                <tr>
-                                    <th scope="row" style={{fontWeight: "1000"}}>Session Chairs</th>
-                                    <td style={{fontWeight: "1000"}}>Shiroman Prakash</td>
-                                    <td style={{fontWeight: "1000"}}>Chandrasekhar Bhamidipati</td>
-                                    <td style={{fontWeight: "1000"}}>Bobby Ezhutachan</td>
-                                    <td/>
-                                    <td style={{fontWeight: "1000"}}>Sheshansu Seskhar Pal</td>
-                                    <td style={{fontWeight: "1000"}}>Kamal Lochan Panigrahi</td>
-                                </tr>
 
+                                </tr>
                                 <tr>
                                     <th scope="row">11:30-12:00</th>
                                     <td>Anshuman Maharana</td>
-                                    <td>Amitabh Virmani (BH hair removal)</td>
-                                    <td>Yuji Tachikawa (Intriligator-Seiberg Duality)</td>
+                                    <td>Amitabh Virmani</td>
+                                    <td>Yuji Tachikawa</td>
                                     <td rowSpan="5"> Free Morning</td>
                                     <td>Shamik Banerjee</td>
-                                    <td>Jaydeep K. Basak (Islands for EN)</td>
+                                    <td>Jaydeep K. Basak</td>
                                 </tr>
                                 <tr>
                                     <th scope="row" rowSpan="2">12:00-12:30</th>
-                                    <td > Suchetan Das (Chaos)</td>
+                                    <td > Suchetan Das</td>
                                     <td > Rajesh Gupta</td>
-                                    <td > Sudip Karan (Log. Corr. to ext.  BH entropy)</td>
-                                    <td > Pranjal Pandey (Modified Celestial Amplitude)</td>
-                                    <td > Akrajyoti Manna (Scatt. Ampl. Of massive vector bosons)</td>
+                                    <td > Sudip Karan</td>
+                                    <td > Pranjal Pandey</td>
+                                    <td > Akrajyoti Manna</td>
                                 </tr>
 
                                 <tr>
-                                    <td > Arnab P. Saha (Scattering)</td>
-                                    <td > Suman Kundu (Bnds - Regge growth)</td>
+                                    <td > Arnab P. Saha</td>
+                                    <td > Suman Kundu</td>
                                     <td > Mohammed Shabir</td>
-                                    <td > Gourav Banerjee (Log. Corr. to BH entropy)</td>
-                                    <td > Sukrut Mondkar (Relax. Dyna.-semi-holog-black branes)</td>
+                                    <td > Gourav Banerjee</td>
+                                    <td > Sukrut Mondkar</td>
                                 </tr>
                                 <tr>
                                     <th scope="row" rowSpan="2">12:30-13:00</th>
-                                    <td > Renjan R. John (Anyon corr, CS)</td>
-                                    <td > Ayan K. Patra (Page Curves and Bath Deformations)</td>
-                                    <td > Madhu Mishra (R^2 Action)</td>
-                                    <td > Aranya Bhattacharya (Islands,page curve,complex.)</td>
+                                    <td > Renjan R. John</td>
+                                    <td > Ayan K. Patra</td>
+                                    <td > Madhu Mishra</td>
+                                    <td > V.Vishal</td>
                                     <td > Debordina Ghosh</td>
                                 </tr>
 
                                 <tr>
                                     <td > Anurag Kaushal</td>
-                                    <td > Sabyasachi Maulik (EE)</td>
-                                    <td > Trakshu Sharma (Scattering AdS)</td>
-                                    <td > Parthiv Haldar (QI to S-Matrix)</td>
-                                    <td > Akhil Sivakumar (Fluct. Hydrodynamics)</td>
+                                    <td > Sabyasachi Maulik</td>
+                                    <td > Trakshu Sharma</td>
+                                    <td > Parthiv Haldar</td>
+                                    <td > Akhil Sivakumar</td>
                                 </tr>
 
                                 <tr style={{background: "rgba(229, 238, 244, 0.6)"}}>
                                     <th scope="row">13:00-14:00</th>
                                     <td colSpan="6">Lunch Break</td>
-                                </tr>
 
-                                <tr>
-                                    <th scope="row" style={{fontWeight: "1000"}}>Session Chairs</th>
-                                    <td style={{fontWeight: "1000"}}>Jyotirmoy Bhattacharya</td>
-                                    <td style={{fontWeight: "1000"}}>Gautam Mandal</td>
-                                    <td style={{fontWeight: "1000"}}>Supriya Kar</td>
-                                    <td style={{fontWeight: "1000"}}>Prasanta Tripathy</td>
-                                    <td style={{fontWeight: "1000"}}>Sudipta Mukherji</td>
-                                    <td style={{fontWeight: "1000"}}>Aninda Sinha</td>
                                 </tr>
-
                                 <tr>
                                     <th scope="row">14:00-14:30</th>
-                                    <td>Abhishek Mehta (3d CFT corr)</td>
-                                    <td rowSpan="3"> Laura Donnay (A celestial road to flat space holography)</td>
-                                    <td>Bindusar Sahoo (Rudiments of SUGRA)</td>
-                                    <td>Shouvik Dutta (Opr growth in 2D CFT)</td>
+                                    <td>Abhishek Mehta</td>
+                                    <td rowSpan="3"> Laura Donnay</td>
+                                    <td>Bindusar Sahoo</td>
+                                    <td>Shouvik Dutta</td>
                                     <td rowSpan="2"> Enrico Pajer</td>
-                                    <td rowSpan="2"> Joao Penedones (The S-matrix Bootstrap reloaded)</td>
+                                    <td rowSpan="2"> Joao Penedones</td>
                                 </tr>
                                 <tr>
                                     <th scope="row" rowSpan="2">14:30-15:00</th>
                                     <td rowSpan="2">Arnab Kundu</td>
-                                    <td rowSpan="2">Suresh Govindarajan (Lie algebras from Umbral MS)</td>
-                                    <td>Aritra Ghosh (Contact  Metr. Structures)</td>
+                                    <td rowSpan="2">Suresh Govindarajan</td>
+                                    <td>Aritra Ghosh</td>
                                 </tr>
                                 <tr>
-                                    <td>Debangshu Mukherjee (2D drop. To 2D YM)</td>
+                                    <td>Debangshu Mukherjee</td>
                                 </tr>
                                 <tr>
                                     <th scope="row" rowSpan="2">15:00-15:30</th>
-                                    <td rowSpan="2">Ayan Mukhopadhyay (Holog, entgmt)</td>
-                                    <td rowSpan="2">Arindam Bhattacharjee (Matrix model with 3D BMS constraints)</td>
-                                    <td rowSpan="2">Chetan Gowdigere (RCFT)</td>
+                                    <td rowSpan="2">Ayan Mukhopadhyay</td>
+                                    <td rowSpan="2">Arindam Bhattacharjee</td>
+                                    <td rowSpan="2">Chetan Gowdigere </td>
                                     <td>Naveen Prabhakar</td>
-                                    <td rowSpan="2">Arpita Mitra (Soft factors in small CC limit)</td>
-                                    <td rowSpan="2">Parijat Dey (Bootstrapping BCFTs)</td>
+                                    <td rowSpan="2">Arpita Mitra</td>
+                                    <td rowSpan="2">Parijat Dey</td>
                                 </tr>
                                 <tr>
-                                    <td>Himanshu Parihar (Page Curve EN)</td>
+                                    <td>Himanshu Parihar</td>
                                 </tr>
                                 <tr style={{background: "rgba(229, 238, 244, 0.6)"}}>
                                     <th scope="row">15:30-16:00</th>
                                     <td colSpan="6">Tea Break</td>
                                 </tr>
-
-                                <tr>
-                                    <th scope="row" style={{fontWeight: "1000"}}>Session Chairs</th>
-                                    <td style={{fontWeight: "1000"}}>Sudhakar Panda</td>
-                                    <td style={{fontWeight: "1000"}}>Sankhadeep Chakraborty</td>
-                                    <td style={{fontWeight: "1000"}}>Sunil Mukhi</td>
-                                    <td style={{fontWeight: "1000"}}>Debashis Ghoshal</td>
-                                    <td style={{fontWeight: "1000"}}>Abhijit Gadde</td>
-                                    <td style={{fontWeight: "1000"}}>Shibaji Roy</td>
-                                </tr>
-
                                 <tr>
                                     <th scope="row">16:00-16:30</th>
-                                    <td>Tadashi Takayanagi (dS holog)</td>
-                                    <td>Mangesh Mandlik (Tensionless Tales: Critical Dimensions and Spectra)</td>
-                                    <td>R. Loganayagam </td>
-                                    <td>Rudranil Basu (Carrollian field theories)</td>
-                                    <td>Sachin Jain (Momentum space-3d CFT corr. Func.)</td>
-                                    <td>Ronak Soni (From Bekenstein-Hawking to Gibbons-Hawking)</td>
+                                    <td>Tadashi Takayanagi</td>
+                                    <td>Mangesh Mandlik</td>
+                                    <td>R. Loganayagam</td>
+                                    <td>Rudranil Basu</td>
+                                    <td>Sachin Jain</td>
+                                    <td>Ronak Soni</td>
                                 </tr>
                                 <tr>
                                     <th scope="row">16:30-17:00</th>
-                                    <td>Krishnan Narayan (Cosmo.,Singula. And QES)</td>
+                                    <td>Krishnan Narayan</td>
                                     <td>Aritra Banerjee</td>
-                                    <td>Debajyoti Sarkar (Endpoint contribution to Modu. Hamiltonian)</td>
+                                    <td>Debajyoti Sarkar</td>
                                     <td>Nilay Kundu</td>
-                                    <td rowSpan="2">Discussion session: Suvrat Raju</td>
+                                    <td rowSpan="2">Discussion session</td>
                                     <td>Siddharth Prabhu</td>
                                 </tr>
                                 <tr>
                                     <th scope="row">17:00-17:30</th>
-                                    <td>Ahmadullah Zahed (crssng sym)</td>
-                                    <td>Kushal Chakraborty (Large N Knot Invariants)</td>
-                                    <td>Karunava Sil (Pole Skipping and chaos)</td>
-                                    <td>Vijay Shenoy (Fractons)</td>
-                                    <td>Aalok Misra (Diff. Geo. Of Flavor Memory in M Theory at O(R^4))</td>
+                                    <td>Ahmadullah Zahed</td>
+                                    <td>Kushal Chakraborty</td>
+                                    <td>Karunava Sil</td>
+                                    <td>Vijay Shenoy</td>
+                                    <td>Aalok Misra</td>
                                 </tr>
                                 <tr>
                                     <th scope="row">17:30-18:00</th>
-                                    <td rowSpan="2"> Discussion session (Chair: Sandip Trivedi)</td>
-                                    <td rowSpan="2"> Discussion session: Dileep Jatkar</td>
-                                    <td >Zohar Komargodski (Line Dfcts and RG Flow)</td>
-                                    <td>Nathan Seiberg (UV/IR mixing)</td>
-                                    <td> Lara Anderson (SU(3) metr from ML)</td>
-                                    <td> Discussion session (Chair: Jnandeva Maharana)</td>
+                                    <td rowSpan="2"> Discussion session</td>
+                                    <td rowSpan="2"> Discussion session</td>
+                                    <td >Zohar Komargodski</td>
+                                    <td>Nathan Seiberg</td>
+                                    <td> Lara Anderson</td>
+                                    <td> Discussion session</td>
                                 </tr>
                                 <tr>
                                     <th scope="row">18:00-18:30</th>
-                                    <td> Discussion session: Sunil Mukhi</td>
+                                    <td> Discussion session</td>
                                     <td>Break</td>
                                     <td>Nemani Suryanarayana</td>
                                 </tr>
@@ -220,7 +181,7 @@ function Timetable() {
                                     <td rowSpan="2"/>
                                     <td rowSpan="2"/>
                                     <td rowSpan="2">Daniel Harlow</td>
-                                    <td>Ahmed Almheiri (Enganglement wedge of couplings)</td>
+                                    <td>Ahmed Almheiri</td>
                                     <td rowSpan="2">Davide Gaiotto</td>
                                     <td rowSpan="2"/>
                                 </tr>
