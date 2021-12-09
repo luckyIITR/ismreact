72f88ba31fc00ebfdbdd606fa521d023de460ad8
mohak300501 <mohak_k@ph.iitr.ac.in>
2021-12-09 13:22:33 +0530

    integrate

--- src/components/Registercompo/RegClosed.js
+++ src/components/Registercompo/RegClosed.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 function RegClosed() {
     return (
-        <div style={{textAlign : 'center', fontSize:'3em', marginTop:'2.5em', minHeight:"60vh", lineHeight: "2", color:'red'}}>
-            Registration Closed
+        <div className="hero" style={{ textAlign: 'center', fontSize: '3em', marginTop: '2.5em', minHeight: "60vh", lineHeight: "2", color: 'red' }}>
+            <div className="register-heading sectionHeading">
+                <br />
+                Registration Closed
+            </div>
         </div>
     )
 }
